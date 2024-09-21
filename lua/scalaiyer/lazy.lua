d7fd569000cef1b259d987a479bae2b2e1c9dcef
Kartik <kartik.aiyer@gmail.com>
2024-09-20 23:28:25 -0700

    Added lazydev and replaced neodev
    I also have to use nvim >= 0.10 everywhere

--- lua/scalaiyer/lazy.lua
+++ lua/scalaiyer/lazy.lua
@@ -1,14 +1,25 @@
 local lazypath = vim.fn.stdpath("data") .. "/lazy/lazy.nvim"
 if not vim.loop.fs_stat(lazypath) then
-	vim.fn.system({
-		"git",
-		"clone",
-		"--filter=blob:none",
-		"https://github.com/folke/lazy.nvim.git",
-		"--branch=stable", -- latest stable release
-		lazypath,
-	})
+  vim.fn.system({
+    "git",
+    "clone",
+    "--filter=blob:none",
+    "https://github.com/folke/lazy.nvim.git",
+    "--branch=stable", -- latest stable release
+    lazypath,
+  })
 end
 vim.opt.rtp:prepend(lazypath)
 
-require("lazy").setup({ { import = "scalaiyer.plugins" }, { import = "scalaiyer.plugins.lsp" } })
+require("lazy").setup({
+  spec = {
+    { import = "scalaiyer.plugins" },
+    { import = "scalaiyer.plugins.lsp" },
+    { "androidsdk.nvim",
+      dev = true,
+    },
+  },
+  dev = {
+    path = "~/fun/nvim_plugins",
+  }
+})
