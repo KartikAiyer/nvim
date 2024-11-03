e77a981f2d2d7f0a0f4eb1595379bdb56eca819d
Kartik Aiyer <kartik.aiyer@gomotive.com>
2024-11-03 09:46:16 -0800

    Updated lspconfig for lua and updated plugins
    Learned a little bit more about lua-ls. It would be better to have a .luarc.json file to configure the language server for each project.

--- lua/scalaiyer/plugins/lsp/lspconfig.lua
+++ lua/scalaiyer/plugins/lsp/lspconfig.lua
@@ -89,23 +89,41 @@ local function configLspConfig()
   lspconfig["lua_ls"].setup({
     capabilities = capabilities,
     on_attach = on_attach,
-    settings = {
-      Lua = {
-        -- make the language server recognize vim global
-        diagnostics = {
-          globals = { "vim" },
+    on_init = function(client)
+      if client.workspace_folders then
+        local path = client.workspace_folders[1].name
+        if vim.uv.fs_stat(path..'/.luarc.json') or vim.uv.fs_stat(path..'/.luarc.jsonc') then
+          return
+        end
+      end
+
+      client.config.settings.Lua = vim.tbl_deep_extend('force', client.config.settings.Lua, {
+        runtime = {
+          -- Tell the language server which version of Lua you're using
+          -- (most likely LuaJIT in the case of Neovim)
+          version = 'LuaJIT'
         },
+        -- Make the server aware of Neovim runtime files
         workspace = {
-          -- make language server aware of runtime files
+          checkThirdParty = false,
           library = {
-            [vim.fn.expand("$VIMRUNTIME/lua")] = true,
-            [vim.fn.stdpath("config") .. "/lua"] = true,
-            [vim.fn.stdpath("config") .. "/lua/plugins"] = true,
-            [vim.fn.stdpath("data") .. "/lazy/plenary.nvim/lua"] = true,
-          },
-        },
-      },
+            vim.env.VIMRUNTIME
+            -- Depending on the usage, you might want to add additional paths here.
+            -- "${3rd}/luv/library"
+            -- "${3rd}/busted/library",
+          }
+          -- or pull in all of 'runtimepath'. NOTE: this is a lot slower and will cause issues when working on your own configuration (see https://github.com/neovim/nvim-lspconfig/issues/3189)
+          -- library = vim.api.nvim_get_runtime_file("", true)
+        }
+      })
+    end,
+    settings = {
+      Lua = {}
     },
+    filetypes={
+      "lua",
+      "lua.bustedspec"
+    }
   })
   lspconfig["cmake"].setup({
     capabilities = capabilities,
