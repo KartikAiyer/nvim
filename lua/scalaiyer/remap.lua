cf401343e368e659f4f28e68e81a860e665cce6a
Kartik Aiyer <kartik.aiyer@gomotive.com>
2024-11-12 15:52:45 -0800

    Updated Harpoon bindings and added quick fix keybindings

--- lua/scalaiyer/remap.lua
+++ lua/scalaiyer/remap.lua
@@ -1,1 +1,6 @@
 vim.g.mapleader = " "
+
+vim.keymap.set('n', '<leader>qo', '<cmd>copen<cr>', {desc = "Open quickfix list"})
+vim.keymap.set('n', '<leader>qw', '<cmd>cprev<cr>', {desc = "Previous quickfix"})
+vim.keymap.set('n', '<leader>qe', '<cmd>cnext<cr>', {desc = "Next quickfix"})
+vim.keymap.set('n', '<leader>qp', '<cmd>cclose<cr>', {desc = "Close quick fix"})
